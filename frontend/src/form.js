1190612fd25675fb8748cc4dcaaec072867e6df5
Noah Amar <noahamar@Noahs-MacBook-Pro.local>
2019-05-01 18:07:01 -0600

    submit button

--- frontend/src/form.js
+++ frontend/src/form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Inputs from './components/inputfield';
+import Button from './components/buttons/button';
 
 class Form extends React.Component {
   render() {
@@ -9,6 +10,12 @@ class Form extends React.Component {
           <p class="welcome">Let's get you all set up so you can verify 
               your information and begin setting up your profile!</p>
         <Inputs />
+        <br />
+        <p class="privacy">I agree to the <a>Terms, Privacy Policy</a> and <a>Fees</a></p>
+        <div class="privacy">
+        <Button />
+        </div>
+        <p></p>
         </div>
     );
   }
