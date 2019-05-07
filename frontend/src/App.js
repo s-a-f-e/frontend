816330b1fd33ac9b106d751677ddb952b9c100f4
Charlie Winslow <cwinslow22@yahoo.com>
2019-05-07 12:10:59 -0500

    accepted changes from git conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import Image from './image';
-import DriverForm from './driverform';
-import MotherForm from './motherform';
-import './App.css';
-<<<<<<< HEAD
-import { Route, Switch, Redirect } from 'react-router-dom';
-=======
+import React from "react";
+import Image from "./image";
+import DriverForm from "./driverform";
+import MotherForm from "./motherform";
+import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-// test comment
->>>>>>> f232336f0bc0ecb3abca4786e3b31a6c102ba301
- 
 class App extends React.Component {
   render() {
     return (
       <div class="row">
         <div class="column-one one">
-        <Image />
+          <Image />
         </div>
 
         <div class="column-two two">
-        <Switch>
-          <Route path='/' exact render ={props => (<DriverForm {...props} />)} />
-          <Route path='/mothers' exact render ={props => (<MotherForm {...props} />)} />
-        </Switch>
+          <Switch>
+            <Route path="/" exact render={props => <DriverForm {...props} />} />
+            <Route
+              path="/mothers"
+              exact
+              render={props => <MotherForm {...props} />}
+            />
+          </Switch>
         </div>
-
-
-        
       </div>
-
     );
   }
 }
