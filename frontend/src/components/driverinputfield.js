979c0cabdbf764ecbcca4eedbe1bb3707f6bd034
Charlie Winslow <cwinslow22@yahoo.com>
2019-05-10 15:14:14 -0500

    added topography component to identify whether the inputs are for mother or driver

--- frontend/src/components/driverinputfield.js
+++ frontend/src/components/driverinputfield.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import MaskedInput from 'react-text-mask';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import axios from 'axios';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import MaskedInput from "react-text-mask";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexDirection: 'column',
-    width: 200,
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "column",
+    width: 200
   },
   textField: {
     marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
+    marginRight: theme.spacing.unit
   },
   dense: {
-    marginTop: 16,
+    marginTop: 16
   },
   menu: {
-    width: 100,
+    width: 100
   },
   button: {
-    margin: theme.spacing.unit,
-  },
+    margin: theme.spacing.unit
+  }
 });
 function TextMaskCustom(props) {
   const { inputRef, ...other } = props;
@@ -42,37 +43,37 @@ function TextMaskCustom(props) {
       }}
       /* prettier-ignore */
       mask={["(",/[1-9]/,/\d/,/\d/,")"," ",/\d/,/\d/,/\d/,"-",/\d/,/\d/,/\d/,/\d/]}
-      placeholderChar={'\u2000'}
+      placeholderChar={"\u2000"}
       showMask
     />
   );
 }
 
 TextMaskCustom.propTypes = {
-  inputRef: PropTypes.func.isRequired,
+  inputRef: PropTypes.func.isRequired
 };
 
 class OutlinedTextFields extends React.Component {
   state = {
-    firstname: '',
-    lastname: '',
-    latitude: '',
-    longitude: '',
-    textmask1: ' ',
-    textmask2: ' ',
-    textmask3: ' ',
+    firstname: "",
+    lastname: "",
+    latitude: "",
+    longitude: "",
+    textmask1: " ",
+    textmask2: " ",
+    textmask3: " "
   };
 
   componentDidMount() {
     const config = {
       username: process.env.REACT_APP_username,
-      password: process.env.REACT_APP_password,
+      password: process.env.REACT_APP_password
     };
-    
+
     axios
-      .post('https://saferides.herokuapp.com/api-token-auth/', config)
+      .post("https://saferides.herokuapp.com/api-token-auth/", config)
       .then(response => {
-        localStorage.setItem('token', 'token ' + response.data.token);
+        localStorage.setItem("token", "token " + response.data.token);
         //console.log(response.data.token);
       });
   }
@@ -88,33 +89,33 @@ class OutlinedTextFields extends React.Component {
 
       await axios
         .post(
-          'https://saferides.herokuapp.com/api/drivers/',
+          "https://saferides.herokuapp.com/api/drivers/",
           request,
-          requestOptions,
+          requestOptions
         )
         .then(res => {
-          console.log('response data: ', res.data);
+          console.log("response data: ", res.data);
         })
         .catch(err => {
-          console.error('axios err:', err);
+          console.error("axios err:", err);
         });
     }
   };
 
   handleSubmit = event => {
     let phones = [];
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
     const requestOptions = {
-      headers: { Authorization: token },
+      headers: { Authorization: token }
     };
 
     const request = {
-      name: this.state.firstname + ' ' + this.state.lastname,
-      phone: '',
+      name: this.state.firstname + " " + this.state.lastname,
+      phone: "",
       latitude: this.state.latitude,
       longitude: this.state.longitude,
-      available: true,
+      available: true
     };
 
     // build the phones array with the given numbers
@@ -135,22 +136,22 @@ class OutlinedTextFields extends React.Component {
 
     // clear state after posted to db
     this.setState({
-      firstname: '',
-      lastname: '',
-      latitude: '',
-      longitude: '',
-      textmask1: ' ',
-      textmask2: ' ',
-      textmask3: ' ',
+      firstname: "",
+      lastname: "",
+      latitude: "",
+      longitude: "",
+      textmask1: " ",
+      textmask2: " ",
+      textmask3: " "
     });
   };
 
   render() {
     const { classes } = this.props;
     const addBtnClass = {
-      display: 'flex',
-      width: '111px',
-      justifyContent: 'center',
+      display: "flex",
+      width: "111px",
+      justifyContent: "center"
     };
 
     return (
@@ -158,6 +159,9 @@ class OutlinedTextFields extends React.Component {
         <Grid item xs={12}>
           <Grid container justify="space-between" spacing={0}>
             <form className={classes.container} noValidate autoComplete="off">
+              <Typography variant="h6" align="center" component="h6">
+                Driver Info
+              </Typography>
               <div class="thename">
                 <TextField
                   id="outlined-name"
@@ -165,7 +169,7 @@ class OutlinedTextFields extends React.Component {
                   name="firstname"
                   className={classes.textField}
                   value={this.state.firstname}
-                  onChange={this.handleChange('name')}
+                  onChange={this.handleChange("name")}
                   margin="normal"
                   variant="outlined"
                 />
@@ -177,7 +181,7 @@ class OutlinedTextFields extends React.Component {
                   name="lastname"
                   className={classes.textField}
                   value={this.state.lastname}
-                  onChange={this.handleChange('name')}
+                  onChange={this.handleChange("name")}
                   margin="normal"
                   variant="outlined"
                 />
@@ -189,7 +193,7 @@ class OutlinedTextFields extends React.Component {
                   name="latitude"
                   className={classes.textField}
                   value={this.state.latitude}
-                  onChange={this.handleChange('name')}
+                  onChange={this.handleChange("name")}
                   margin="normal"
                   variant="outlined"
                 />
@@ -200,7 +204,7 @@ class OutlinedTextFields extends React.Component {
                     name="longitude"
                     className={classes.textField}
                     value={this.state.longitude}
-                    onChange={this.handleChange('name')}
+                    onChange={this.handleChange("name")}
                     margin="normal"
                     variant="outlined"
                   />
@@ -213,7 +217,7 @@ class OutlinedTextFields extends React.Component {
                 <Input
                   value={this.state.textmask1}
                   name="textmask1"
-                  onChange={this.handleChange('name')}
+                  onChange={this.handleChange("name")}
                   id="formatted-text-mask-input"
                   inputComponent={TextMaskCustom}
                 />
@@ -226,7 +230,7 @@ class OutlinedTextFields extends React.Component {
                 <Input
                   value={this.state.textmask2}
                   name="textmask2"
-                  onChange={this.handleChange('name')}
+                  onChange={this.handleChange("name")}
                   id="formatted-text-mask-input"
                   inputComponent={TextMaskCustom}
                 />
@@ -234,13 +238,13 @@ class OutlinedTextFields extends React.Component {
 
               <FormControl className={classes.formControl} class="phone3">
                 <InputLabel htmlFor="formatted-text-mask-input">
-                  {' '}
+                  {" "}
                   tertiary phone number
                 </InputLabel>
                 <Input
                   value={this.state.textmask3}
                   name="textmask3"
-                  onChange={this.handleChange('name')}
+                  onChange={this.handleChange("name")}
                   id="formatted-text-mask-input"
                   inputComponent={TextMaskCustom}
                 />
@@ -266,7 +270,7 @@ class OutlinedTextFields extends React.Component {
 }
 
 OutlinedTextFields.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(OutlinedTextFields);
