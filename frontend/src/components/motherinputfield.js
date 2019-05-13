5dd7f59426753be890e83f438e2807249a62c2b7
Charlie Winslow <cwinslow22@yahoo.com>
2019-05-13 18:24:50 -0500

    updated the phone number field for mother input to match format Jessica told us

--- frontend/src/components/motherinputfield.js
+++ frontend/src/components/motherinputfield.js
@@ -42,16 +42,14 @@ function TextMaskCustom(props) {
         inputRef(ref ? ref.inputElement : null);
       }}
       mask={[
-        '(',
-        /[1-9]/,
         /\d/,
         /\d/,
-        ')',
+        /\d/,
+        ' ',
+        '-',
         ' ',
         /\d/,
         /\d/,
-        /\d/,
-        '-',
         /\d/,
         /\d/,
         /\d/,
