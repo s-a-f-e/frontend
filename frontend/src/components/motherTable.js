138fceacee301ae27afb144a09ed3d8801a1c278
Anthony Greb <agreb255@gmail.com>
2019-06-15 10:07:38 -0400

    starting on hooks

--- frontend/src/components/motherTable.js
+++ frontend/src/components/motherTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { lighten, makeStyles } from '@material-ui/core/styles';
