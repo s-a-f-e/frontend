59ea5041283d43ceae4cace3ad5d3598c3e20e25
Anthony Greb <agreb255@gmail.com>
2019-06-24 19:06:43 -0400

    trying some things

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class Login extends React.Component {
-    state = {};
+    state = {
+      username: "",
+      password: ""
+    };
     render() {
         return (
-            <div> login </div>
+          <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+              type="text"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              type="password"
+            />
+          </div>
+          <div>
+            <button type="submit">Signin</button>
+          </div>
+        </form>
         )
     }
-    isAuthenticated() {
-        let expiresAt = JSON.parse(localStorage("expires_at"));
-        return new Date().getTime() < expiresAt;
-      }
+    handleInputChange = (e) => {
+      const { name, value } = e.target;
+      this.setState({ [name]: value });
+  }
+  
+  handleSubmit = (e) => {
+      e.preventDefault();
+
+      const endpoint = "https://rideforsafelife.org/login";
+
+      axios
+          .post(endpoint, this.state)
+          .then(res => {
+              localStorage.setItem();
+          })
+          .catch(err => {
+              console.log("ERROR", err);
+          })
+  }
 }
 
 export default Login;
