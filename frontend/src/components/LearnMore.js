1d4539d0f885e8fc6180d9b1b103652244e2c532
Anthony Greb <agreb255@gmail.com>
2019-06-27 14:27:10 -0400

    added img with opacity to learn more

--- frontend/src/components/LearnMore.js
+++ frontend/src/components/LearnMore.js
@@ -4,51 +4,56 @@ import Button from "@material-ui/core/Button";
 import {Link} from 'react-router-dom';
 import Divider from '@material-ui/core/Divider';
 
+
 export default class LearnMore extends Component {
     render() {
         return (
             <div class = 'learn-page'>
-            <div class = 'learn'>
-            </div>
-            <div class = 'learn-content'>
-
-                <div class = 'learn-title'>
-                    <Typography variant="h3">What does SAFE Do?</Typography>
-                    <Divider variant="middle" />
+                <div class = 'learn'>
                 </div>
-               
-                
-                <Typography variant="h5">
-                Safe Mothers, Safe Babies (SAFE) believes that no woman should die while bringing life into the world,
-                and that no child should die when his/her life is just beginning. We also believe in the capacity and
-                ingenuity of the rural poor. Together, we partner to improve the "Three Delays" that collectively lead
-                to maternal and child death. We use our innovative, community-based model to ensure that mothers and
-                children in the first 1,000 days of life lead healthy, empowered lives.​
-                </Typography>
-            </div>
-            <div class = 'learn-content'>
+                    <div className="safe-logo-div">
 
-                <div class = 'learn-title'>
-                    <Typography variant="h3">Why SAFE Works</Typography>
-                    <Divider variant="middle" />
+                    <div class = 'learn-content'>
+                    
+                        <div class = 'learn-title'>
+                            <Typography variant="h3">What does SAFE Do?</Typography>
+                            <Divider variant="middle"/>
+                        </div>
+                    
+                        
+                        <Typography variant="h5">
+                        Safe Mothers, Safe Babies (SAFE) believes that no woman should die while bringing life into the world,
+                        and that no child should die when his/her life is just beginning. We also believe in the capacity and
+                        ingenuity of the rural poor. Together, we partner to improve the "Three Delays" that collectively lead
+                        to maternal and child death. We use our innovative, community-based model to ensure that mothers and
+                        children in the first 1,000 days of life lead healthy, empowered lives.​
+                        </Typography>
+                    </div>
+                    <div class = 'learn-content'>
+
+                        <div class = 'learn-title'>
+                            <Typography variant="h3">Why SAFE Works</Typography>
+                            <Divider variant="middle" />
+                        </div>
+                        
+                        <Typography variant="h5">
+                        SAFE prides itself on being evidence-based and successful. Our model is focused on the provision of
+                        evidence-based public health and medical interventions that are also adaptive and engage target communities
+                        directly in identifying and sustainably solving the problems that impact the survival of women, children,
+                        and families in their communities. We place significant emphasis on evaluating our work to understand what's
+                        working and what's not and contributing to scientific literature to push our field forward.
+                        </Typography>
+                    </div>
+                    </div>
+                    <div class = 'learn-button'>
+                    <Link
+                    style={{ textDecoration: 'none'}}
+                    to="/">
+                        <Button color="primary" variant="contained">Return Home</Button>
+                    </Link>
+                    </div>
                 </div>
-                
-                <Typography variant="h5">
-                SAFE prides itself on being evidence-based and successful. Our model is focused on the provision of
-                evidence-based public health and medical interventions that are also adaptive and engage target communities
-                directly in identifying and sustainably solving the problems that impact the survival of women, children,
-                and families in their communities. We place significant emphasis on evaluating our work to understand what's
-                working and what's not and contributing to scientific literature to push our field forward.
-                </Typography>
-            </div>
-            <div class = 'learn-button'>
-            <Link
-             style={{ textDecoration: 'none'}}
-              to="/">
-                <Button color="primary" variant="contained">Return Home</Button>
-            </Link>
-            </div>
-            </div>
+            
         )
     }
 }
